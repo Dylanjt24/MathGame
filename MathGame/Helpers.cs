7100c42d255b4e3260862c10fb626a3f6a2e3fe6
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-01-18 09:25:14 -0800

    Created Helper class, changed other classes to use static Helpers methods

---
+++ MathGame/Helpers.cs
@@ -0,0 +1,49 @@
+﻿namespace MathGame
+{
+    internal class Helpers
+    {
+        // Get and show scores of previous games
+        internal static void GetGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Games History");
+            Console.WriteLine("------------------------------------------");
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
+            Console.WriteLine("------------------------------------------\n");
+            Console.WriteLine("Press enter to return to the main menu");
+            Console.ReadLine();
+        }
+
+        // Add the datetime, game type, and score to game history list
+        internal static void AddToHistory(int score, string gameType)
+        {
+            games.Add($"{DateTime.Now} - {gameType}: Score = {score}");
+        }
+
+        // Get numbers for division game
+        internal static int[] GetDivisionNumbers()
+        {
+            var random = new Random();
+            var firstNumber = random.Next(1, 101);
+            var secondNumber = random.Next(1, 101);
+
+            // Generate numbers until they are evenly divisible
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 101);
+                secondNumber = random.Next(1, 101);
+            }
+            int[] result = { firstNumber, secondNumber };
+
+            return result;
+        }
+
+        internal static void Quit()
+        {
+            Console.WriteLine("See ya later!");
+        }
+    }
+}
