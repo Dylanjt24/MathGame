2d38ec9b2c5f3275e23016b9dfba4c2f9d522f79
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-01-18 09:08:12 -0800

    Created Menu class and moved Menu method from Program.cs to Menu.cs

--- MathGame/Menu.cs
+++ MathGame/Menu.cs
@@ -8,5 +8,55 @@ namespace MathGame
 {
     internal class Menu
     {
+        internal void ShowMenu(string? name)
+        {
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your maths game. That's great you're working on improving yourself. \n");
+
+            // Keeps track of whether the game has ended or not
+            var isGameOn = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($@"What game would you like to play today? Choose from the options below:
+            V - View Previous Games
+            A - Addition
+            S - Subtraction
+            M - Multiplication
+            D - Division
+            Q - Quit the program");
+                Console.WriteLine("------------------------------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        GetGames();
+                        break;
+                    case "a":
+                        AdditionGame("Addition game");
+                        break;
+                    case "s":
+                        SubtractionGame("Subtraction game");
+                        break;
+                    case "m":
+                        MultiplicationGame("Multiplication game");
+                        break;
+                    case "d":
+                        DivisionGame("Division game");
+                        break;
+                    case "q":
+                        // set isGameOn to false when player quits so it breaks the do-while loop
+                        isGameOn = false;
+                        Quit();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.\n");
+                        break;
+                }
+            } while (isGameOn);
+        }
     }
 }
