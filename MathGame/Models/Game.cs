79e8404b4d90930eee76c863ee482398a35cf32b
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-02-02 09:24:00 -0800

    Created Models folder and Game model

---
+++ MathGame/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace MathGame.Models;
+
+internal class Game
+{
+    /*private int _score;
+
+    public int Score
+    {
+        get { return _score; }
+        set { _score = value; }
+    }*/
+
+    // Auto property - does the same thing as the code above with the backing field, just with fewer lines of code
+    public int Score { get; set; }
+    public DateTime Date { get; set; }
+    public string Type { get; set; }
+}
