c4bc0521c3161d99c897c314d18df21f4e64b747
Dylan Thierbach <44076048+Dylanjt24@users.noreply.github.com>
2024-02-16 09:23:21 -0800

    Created initial ChooseDifficulty method

--- MathGame/Helpers.cs
+++ MathGame/Helpers.cs
@@ -6,19 +6,19 @@ namespace MathGame
     {
         internal static List<Game> games = new List<Game>
         {
-            new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
-            new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
-            new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
-            new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
-            new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
-            new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
-            new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
-            new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
-            new Game { Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4 },
-            new Game { Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1 },
-            new Game { Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0 },
-            new Game { Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2 },
-            new Game { Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5 },
+            //new Game { Date = DateTime.Now.AddDays(1), Type = GameType.Addition, Score = 5 },
+            //new Game { Date = DateTime.Now.AddDays(2), Type = GameType.Multiplication, Score = 4 },
+            //new Game { Date = DateTime.Now.AddDays(3), Type = GameType.Division, Score = 4 },
+            //new Game { Date = DateTime.Now.AddDays(4), Type = GameType.Subtraction, Score = 3 },
+            //new Game { Date = DateTime.Now.AddDays(5), Type = GameType.Addition, Score = 1 },
+            //new Game { Date = DateTime.Now.AddDays(6), Type = GameType.Multiplication, Score = 2 },
+            //new Game { Date = DateTime.Now.AddDays(7), Type = GameType.Division, Score = 3 },
+            //new Game { Date = DateTime.Now.AddDays(8), Type = GameType.Subtraction, Score = 4 },
+            //new Game { Date = DateTime.Now.AddDays(9), Type = GameType.Addition, Score = 4 },
+            //new Game { Date = DateTime.Now.AddDays(10), Type = GameType.Multiplication, Score = 1 },
+            //new Game { Date = DateTime.Now.AddDays(11), Type = GameType.Subtraction, Score = 0 },
+            //new Game { Date = DateTime.Now.AddDays(12), Type = GameType.Division, Score = 2 },
+            //new Game { Date = DateTime.Now.AddDays(13), Type = GameType.Subtraction, Score = 5 },
         };
 
         // Get and show scores of previous games
@@ -27,12 +27,12 @@ namespace MathGame
             // LINQ Query examples:
             //var gamesToPrint = games.Where(x => x.Type == GameType.Division);
             //var gamesToPrint = games.Where(x => x.Date > new DateTime(2024, 02, 10) && x.Score > 3);
-            var gamesToPrint = games.Where(x => x.Date > new DateTime(2024, 02, 10)).OrderByDescending(x => x.Score);
+            //var gamesToPrint = games.Where(x => x.Date > new DateTime(2024, 02, 10)).OrderByDescending(x => x.Score);
 
             Console.Clear();
             Console.WriteLine("Games History");
             Console.WriteLine("------------------------------------------");
-            foreach (var game in gamesToPrint)
+            foreach (var game in games)
             {
                 Console.WriteLine($"{game.Date} - {game.Type}: {game.Score} pts");
             }
@@ -99,6 +99,36 @@ namespace MathGame
             }
 
             return name;
+        }
+
+        internal static int[] ChooseDifficulty()
+        {
+            Console.WriteLine(@"Choose a difficulty:
+            E - Easy
+            M - Medium
+            H - Hard
+            ------------------------------------------");
+            var difficulty = Console.ReadLine();
+            var mathNums = new int[2];
+            mathNums[0] = 1;
+
+            switch (difficulty.Trim().ToLower())
+            {
+                case "e":
+                    mathNums[1] = 10;
+                    break;
+                case "m":
+                    mathNums[1] = 50;
+                    break;
+                case "h":
+                    mathNums[1] = 100;
+                    break;
+                default:
+                    Console.WriteLine("Invalid input.\n");
+                    break;
+            }
+            return mathNums;
+
         }
     }
 }
